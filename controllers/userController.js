7c6599642e5d48cdb9e88a7c20889fe24522f501
sumanthpweb11 <sumanthpweb11@gmail.com>
2022-12-02 20:17:11 +0530

    minor changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -338,7 +338,7 @@ User.watch().on("change", async () => {
 
   const subscription = await User.find({ "subscription.status": "active" });
 
-  stats[0].users = User.countDocuments();
+  stats[0].users = await User.countDocuments();
   stats[0].subscription = subscription.length;
   stats[0].createdAt = new Date(Date.now());
 
