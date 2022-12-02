a3372f0d355d2d78477504a61035495252adfc77
sumanthpweb11 <sumanthpweb11@gmail.com>
2022-12-02 19:49:09 +0530

    changed token expiry

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,7 +74,7 @@ export const logout = catchAsyncError(async (req, res, next) => {
   // };
 
   const options = {
-    expires: new Date(Date.now()),
+    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
     sameSite: "none",
